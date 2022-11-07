d13374abf13c75ec5c453a0a82b82dc55c44e595
EOn9in <117629350+EOn9in@users.noreply.github.com>
2022-11-07 10:43:43 +0300

    Add files via upload

---
+++ ДКР №1/Z1.pas
@@ -0,0 +1,9 @@
+﻿program z1_lr1;
+var x,func:real;
+begin
+  read(x);
+  if (x<-10) then func:=power(e,x) * power(e,x) + sin (x)/power(x,3);
+  if (x<=-4)and (x>=-10) then func:=power(x,0.1 * x)- power(e,x);
+  if (x>-4) then func:=sqr(x)*ln(x) - power(x,3);
+   writeln('y = ',func:4:2);
+end. 
---
+++ ДКР №1/Z2.pas
@@ -0,0 +1,14 @@
+﻿program z1_lr1;
+var a,b,h,x,func:real;
+begin
+  read(a,b,h);
+  x:=a;
+  while x<=b do
+    begin 
+  if (x<-10) then func:=power(e,x) * power(e,x) + (sin (x)/power(x,3));
+  if (x<=-4) and (x>=-10) then func:=power(x,0.1 * x)- power(e,x);
+  if x>-4 then func:=sqr(x)*ln(x) -power(x,3);
+  writeln('x = ',x,'  y = ',func:3:2);
+   x:=x+h;
+   end;
+end.
